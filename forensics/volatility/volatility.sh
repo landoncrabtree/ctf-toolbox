dfbdd6adf0f7b04dfc56a4b894fb999b7fc8b678
Landon Crabtree <me@landon.pw>
2023-10-10 19:50:59 -0500

    Update volatility.sh

--- forensics/volatility/volatility.sh
+++ forensics/volatility/volatility.sh
@@ -1,55 +1,44 @@
-# username=$(whoami)
-
-# mkdir -p ~/.local/bin
-
-# sudo apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
-# sudo apt install -y python2 python2.7-dev libpython2-dev
-# curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-# sudo python2 get-pip.py
-# sudo python2 -m pip install -U setuptools wheel
-# python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
-# sudo python2 -m pip install yara
-# sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
-# python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
-# mv ~/.local/bin/vol.py ~/.local/bin/vol2.py
-
-# sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
-# python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
-# python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
-# mv ~/.local/bin/vol ~/.local/bin/vol3.py
-
-# echo 'export PATH=/home/'"$username"'/.local/bin:$PATH' >> ~/.bashrc
-# echo 'export PATH=/home/'"$username"'/.local/bin:$PATH' >> ~/.zshrc
-
-# echo 'Close this shell and open a new one!'
-# echo 'vol2.py for volatility2 and vol3.py for volatility3'
-
+# Install Volatility2
+# First, we need to install Python2 and Python2 PIP
+# Then install all dependencies
 mkdir -p ~/Downloads/vol2
 cd ~/Downloads/vol2
-
 sudo apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
 sudo apt install -y python2 python2.7-dev libpython2-dev python2-dev
-
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 rm get-pip.py
-sudo python2 -m pip install -U setuptools wheel
-sudo python2 -m pip install virtualenv
+sudo python2 -m pip install -U setuptools wheel virtualenv
+#python2 -m virtualenv ~/volatility2_venv
+#source ~/volatility2_venv/bin/activate
+sudo python2 -m pip install -U distorm3 pycrypto yara
+sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+#ln -s ~/volatility2_venv/lib/python2.7/site-packages/usr/lib/libyara.so ~/volatility2_venv/lib/libyara.so
+git clone https://github.com/volatilityfoundation/volatility.git ~/volatility2
+cd ~/volatility2
+sudo python2 setup.py install
 
-virtualenv -p python2 ~/volatility2
-source ~/volatility2/bin/activate
+# Install Volatility3
+sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
+sudo python3 -m pip install -U distorm3 yara pycryptodome pillow openpyxl ujson pytz ipython capstone
+git clone https://github.com/volatilityfoundation/volatility3.git ~/volatility3
+cd ~/volatility3
+sudo python3 setup.py install
 
-python2 -m pip install -U distorm3 pycrypto
-python2 -m pip install -U yara pillow openpyxl ujson pytz ipython capstone
+# Add Volatility to PATH
+echo 'export PATH=~/volatility2:$PATH' >> ~/.bashrc
+echo 'export PATH=~/volatility3:$PATH' >> ~/.bashrc
+echo 'export PATH=~/volatility2:$PATH' >> ~/.zshrc
+echo 'export PATH=~/volatility3:$PATH' >> ~/.zshrc
 
-git clone https://github.com/volatilityfoundation/volatility.git
-cd volatility
-python2 setup.py install
+# Fix shebang so loader knows to use it with Python 2.7
+sed -i '1s|.*|#!/usr/bin/env python2.7|' ~/volatility2/vol.py
 
-echo <<<EOF
-#!/bin/bash
-source ~/volatility2/bin/activate
-
-EOF >> 
-
-
+# Setup linking so we can use vol2.py and vol3.py
+# Setup alias so we can also call just vol2 & vol3
+ln -s ~/volatility2/vol.py ~/volatility2/vol2.py && chmod +x $_
+ln -s ~/volatility2/vol.py ~/volatility3/vol3.py && chmod +x $_
+echo 'alias vol2="vol2.py"' >> ~/.bashrc
+echo 'alias vol3="vol3.py"' >> ~/.bashrc
+echo 'alias vol2="vol2.py"' >> ~/.zshrc
+echo 'alias vol3="vol3.py"' >> ~/.zshrc
