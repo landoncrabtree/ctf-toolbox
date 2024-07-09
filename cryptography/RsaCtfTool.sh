da0fbe7b677b452c3ec2008e55a00a7ab7b469ae
Landon Crabtree <me@landon.pw>
2024-07-09 09:08:26 -0500

    Update RsaCtfTool.sh

--- cryptography/RsaCtfTool.sh
+++ cryptography/RsaCtfTool.sh
@@ -2,6 +2,6 @@ username=$(whoami)
 
 sudo apt install libgmp3-dev libmpc-dev
 sudo git clone https://github.com/RsaCtfTool/RsaCtfTool.git /usr/local/bin/RsaCtfTool
-python3 -m pip install -r /usr/local/bin/RsaCtfTool/requirements.txt --user
+python3 -m pip install -r /usr/local/bin/RsaCtfTool/requirements.txt --user --break-system-packages
 echo 'export PATH=/home/'"$username"'/usr/local/bin/RsaCtfTool:PATH' >> ~/.zshrc
 echo 'export PATH=/home/'"$username"'/usr/local/bin/RsaCtfTool:$PATH' >> ~/.bashrc
