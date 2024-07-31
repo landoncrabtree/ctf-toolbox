85eea9ea2de3cfc46011cb2f1c2d2a9375ceef77
Landon Crabtree <me@landon.pw>
2024-07-31 12:33:48 -0700

    Update volatility.sh

--- forensics/volatility/volatility.sh
+++ forensics/volatility/volatility.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # This is needed for the script to work on ubuntu. works on kali without. 
-sudo apt install python3-pip curl -y
+sudo apt install python3-pip curl libpcre3 -y
 
 sudo pip install -U setuptools
 
